92b6e3d20cd61aaf7f81ffcf74b4eb67ee4f7887
polishPKM <42143807+polishPKM@users.noreply.github.com>
2021-10-30 18:54:54 +0700

    Add files via upload

---
+++ Delay.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 29 18:45:33 2021
+
+@author: ibure
+"""
+
+import time
+print("Test")
+time.sleep(5)
+print("This message will be printed after a wait of 5 seconds")
